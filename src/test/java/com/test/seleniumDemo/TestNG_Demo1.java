4a240380b090f7f7b170f47ea747a7a8c2887b81
guanhejiuzhou <18021055980@163.com>
2019-09-08 21:01:21 +0800

    TestNG第一次使用

---
+++ src/test/java/com/test/seleniumDemo/TestNG_Demo1.java
@@ -0,0 +1,32 @@
+package com.test.seleniumDemo;
+
+
+import org.testng.annotations.*;
+
+/**
+ * @author 关河九州
+ * @date 2019/9/8 0008 20:19
+ * @version 1.0
+ */
+public class TestNG_Demo1 {
+    @Test
+    public void test1(){
+        System.out.println("1=================");
+    }
+    @Test
+    public void test2(){
+        System.out.println("2=================");
+    }
+    @Test
+    public void test3(){
+        System.out.println("3=================");
+    }
+    @Test
+    public void test4(){
+        System.out.println("4=================");
+    }
+    @Test
+    public void test5(){
+        System.out.println("5=================");
+    }
+}
